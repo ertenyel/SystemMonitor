b94d401d1275c9abeac6eed42da40332e1521bff
Кирилл Богданов <bogdanovkirill2000@mail.ru>
2021-12-30 12:46:56 +0300

    Первое редактирование

--- SystemMonitor/Form1.cs
+++ SystemMonitor/Form1.cs
@@ -6,10 +6,10 @@ using System.Windows.Forms;
 namespace SystemMonitor
 {
     public partial class Form1 : Form
-    {
-        DateTime dateTime1;
-        int itemsCount = 0;
-        int i;
+    {        
+        double recSegmentsValue;
+        double sentSegmentsValue;
+        int itemsCount = 0;       
 
         public Form1()
         {
@@ -17,6 +17,16 @@ namespace SystemMonitor
         }
 
         private void TimerProcLoad_Tick(object sender, EventArgs e)
+        {
+            SystemResourses();
+            InitializeParameters(ProcessLoadBar.Value, PhisycalDiskBar.Value, MemoryLoadBar.Value, itemsCount, (int)Math.Round(recSegmentsValue), (int)Math.Round(sentSegmentsValue));
+            ShowActiveTcpConnections();
+            SecurityLog();
+        }
+        /// <summary>
+        /// Work with system resourses
+        /// </summary>
+        public void SystemResourses()
         {
             ProcessLoadBar.Value = (int)Math.Round(Processor.NextValue());
             try
@@ -28,22 +38,45 @@ namespace SystemMonitor
                 PhisycalDiskBar.Value = 0;
                 MessageBox.Show(ex.Message);
             }
-
             MemoryLoadBar.Value = (int)(Math.Round(Memory.NextValue()));
             ProcessLoadLabel.Text = "Processor load " + ProcessLoadBar.Value + " %";
             LabelPhysicalDisk.Text = "Phisycal disk load " + PhisycalDiskBar.Value + " %";
-            double recSegmentsValue = BytesReceived.NextValue();
-            double sentSegmentsValue = SentBytes.NextValue();
+        }
+        /// <summary>
+        /// Work with servers security 
+        /// </summary>
+        public void SecurityLog()
+        {
+            LabelLogonServer.Text = "Logon to server: " + CounterLogonServer.NextValue() + " Logon/sec";
+            LabelErrorLogon.Text = "Error logon to server: " + CounterErrorLogon.NextValue();
+        }
+        /// <summary>
+        /// Sending active tcp connections
+        /// </summary>
+        public void ShowActiveTcpConnections()
+        {
+            recSegmentsValue = BytesReceived.NextValue();
+            sentSegmentsValue = SentBytes.NextValue();
             LabelMemoryLoad.Text = "Memory load " + MemoryLoadBar.Value + " %";
             ReceivedBytesLabel.Text = "Received bytes: " + recSegmentsValue + " bytes/sec";
             BytesSentLabel.Text = "Sent bytes: " + sentSegmentsValue + " bytes/sec";
-            LabelLogonServer.Text = "Logon to server: " + CounterLogonServer.NextValue() + " Logon/sec";
-            LabelErrorLogon.Text = "Error logon to server: " + CounterErrorLogon.NextValue();
 
-            InitializeParameters(ProcessLoadBar.Value, PhisycalDiskBar.Value, MemoryLoadBar.Value, itemsCount, (int)Math.Round(recSegmentsValue), (int)Math.Round(sentSegmentsValue));
-            ShowActiveTcpConnections();
-            //SecurityLog();
+            itemsCount = 0;
+            listBox1.Items.Clear();
+            TcpConnectionInformation[] connections = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections();
+            foreach (var c in connections)
+                listBox1.Items.Add($"{itemsCount++}. >> \t" + $" Local connection\t{c.LocalEndPoint} <==> {c.RemoteEndPoint}\t remoute connection");
+            LabelItemsCount.Text = "Connections count: " + itemsCount;
         }
+        /// <summary>
+        /// Parameters for chart
+        /// </summary>
+        /// <param name="valueCPUX"></param>
+        /// <param name="valueDiscX"></param>
+        /// <param name="valueMemX"></param>
+        /// <param name="valueConX"></param>
+        /// <param name="valueConRecX"></param>
+        /// <param name="valueConSentX"></param>
         public void InitializeParameters(int valueCPUX, int valueDiscX, int valueMemX, int valueConX, int valueConRecX, int valueConSentX)
         {
             Chart.FillChart(valueCPUX, valueDiscX, valueMemX, valueConX, valueConRecX, valueConSentX);
@@ -67,21 +100,6 @@ namespace SystemMonitor
                 ChartForTCPCon.Series["Received bytes"].Points.AddXY(Chart.conRecX[k], Chart.conRexY[k]);
                 ChartForTCPCon.Series["Sent bytes"].Points.AddXY(Chart.conSentX[k], Chart.conSentY[k]);
             }
-        }
-
-        public void SecurityLog()
-        {
-
-        }
-
-        public void ShowActiveTcpConnections()
-        {
-            itemsCount = 0;
-            listBox1.Items.Clear();
-            TcpConnectionInformation[] connections = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections();
-            foreach (var c in connections)
-                listBox1.Items.Add($"{itemsCount++}. >> \t" + $" Local connection\t{c.LocalEndPoint} <==> {c.RemoteEndPoint}\t remoute connection");
-            LabelItemsCount.Text = "Connections count: " + itemsCount;
         }
     }
 }
